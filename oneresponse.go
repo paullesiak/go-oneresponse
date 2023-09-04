86af799da1dcb5620b3b44f69d9dd80cde77c4cb
Paul Lesiak <paul@paullesiak.com>
2023-09-04 17:55:52 -0400

    Initial commit

--- oneresponse.go
+++ oneresponse.go
@@ -3,23 +3,15 @@ package oneresponse
 import (
 	"errors"
 	"sync/atomic"
-
-	"github.com/rs/zerolog/log"
 )
-
-/*
-oneresponse will call multiple functions with the same signature, and return the first one that returns a non-error
-response, otherwise will return a nil value with a concatenated list of errors. any non-initial response will be dropped
-*/
 
 // OperationWithData is a generic function type that will allow some function to return a response and an error
 type OperationWithData[T any] func() (T, error)
 
-// Serial will call multiple functions passed in with the same signature, and return the first one that gives a positive
-// reply in a serial order
+// Serial will call multiple functions passed in with the same signature, and return the first one that gives a
+// non-error response in the order passed
 func Serial[T any](operation []OperationWithData[T]) (T, error) {
 	var errs []error
-	success := atomic.Bool{}
 	var res T
 	for _, op := range operation {
 		var err error
@@ -28,49 +20,39 @@ func Serial[T any](operation []OperationWithData[T]) (T, error) {
 			errs = append(errs, err)
 			continue
 		}
-		success.Store(true)
-		return res, nil
-	}
-	if success.Load() {
 		return res, nil
 	}
 	return res, errors.Join(errs...)
 }
 
-// Parallel will call multiple functions passed in with the same signature, and return the first one that gives
-// a positive reply in a sequential order
+// Parallel will call multiple functions passed in with the same signature in parallel and return the value for the
+// first one that returns a non-error response. If no function returns successfully, a joined list of errors will be
+// returned.
 func Parallel[T any](operation []OperationWithData[T]) (T, error) {
 	var errs []error
 	var result T
 	var success atomic.Bool
 	resCh := make(chan T, len(operation))
 	errCh := make(chan error, len(operation))
-	log.Info().Msg("Starting operations")
 	for i := range operation {
 		go func(index int, o OperationWithData[T]) {
 			res, err := o()
 			if err != nil {
-				log.Error().Err(err).Msgf("operation %d failed", index)
 				errCh <- err
 				return
 			}
 			success.Store(true)
-			log.Info().Interface("result", res).Msgf("operation %d succeeded", index)
 			resCh <- res
 		}(i, operation[i])
 	}
-	log.Info().Msg("Waiting for responses")
 consumeLoop:
 	for {
 		select {
 		case result = <-resCh:
-			log.Info().Msg("got a response")
 			break consumeLoop
 		case err := <-errCh:
 			errs = append(errs, err)
-			log.Error().Msg("got an error")
 			if len(errs) == len(operation) {
-				log.Error().Msg("errors for all operations")
 				break consumeLoop
 			}
 		}
--- oneresponse_test.go
+++ oneresponse_test.go
@@ -4,36 +4,36 @@ import (
 	"errors"
 	"fmt"
 	"testing"
-	"time"
 
 	_ "github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-// Test functions and errors
 var (
-	err2             = errors.New("some error")
-	err3             = errors.New("another error")
-	successBoolFunc1 = func() (bool, error) {
-		return true, nil
-	}
-	failingBoolFunc1 = func() (bool, error) {
-		return false, err2
-	}
-	failingBoolFunc2 = func() (bool, error) {
-		return false, err3
-	}
+	err2 = errors.New("some error")
+	err3 = errors.New("another error")
 )
+
+func successBoolFunc1() (bool, error) {
+	return true, nil
+}
+func failingBoolFunc1() (bool, error) {
+	return false, err2
+}
+func failingBoolFunc2() (bool, error) {
+	return false, err3
+}
 
 func TestOneResponseSerial(t *testing.T) {
 	type args[T any] struct {
 		operation []OperationWithData[T]
 	}
 	type testCase[T any] struct {
-		name    string
-		args    args[T]
-		want    T
-		wantErr bool
+		name        string
+		args        args[T]
+		want        T
+		wantErr     bool
+		expectedErr []*error
 	}
 	tests := []testCase[bool]{
 		{
@@ -49,8 +49,9 @@ func TestOneResponseSerial(t *testing.T) {
 			args: args[bool]{
 				operation: []OperationWithData[bool]{failingBoolFunc1, failingBoolFunc2},
 			},
-			want:    false,
-			wantErr: true,
+			want:        false,
+			wantErr:     true,
+			expectedErr: []*error{&err2, &err3},
 		},
 	}
 	for _, tt := range tests {
@@ -61,7 +62,9 @@ func TestOneResponseSerial(t *testing.T) {
 			}
 			require.Equalf(t, tt.want, got, "Serial(%v)", tt.args.operation)
 			if tt.wantErr {
-				require.Equal(t, errors.Join(err2, err3), err)
+				for _, e := range tt.expectedErr {
+					require.ErrorAs(t, err, e)
+				}
 			}
 		})
 	}
@@ -72,10 +75,11 @@ func TestOneResponseParallel(t *testing.T) {
 		operation []OperationWithData[T]
 	}
 	type testCase[T any] struct {
-		name    string
-		args    args[T]
-		want    T
-		wantErr bool
+		name        string
+		args        args[T]
+		want        T
+		wantErr     bool
+		expectedErr []*error
 	}
 	tests := []testCase[bool]{
 		{
@@ -91,8 +95,9 @@ func TestOneResponseParallel(t *testing.T) {
 			args: args[bool]{
 				operation: []OperationWithData[bool]{failingBoolFunc1, failingBoolFunc2},
 			},
-			want:    false,
-			wantErr: true,
+			want:        false,
+			wantErr:     true,
+			expectedErr: []*error{&err2, &err3},
 		},
 	}
 	for _, tt := range tests {
@@ -103,12 +108,10 @@ func TestOneResponseParallel(t *testing.T) {
 			}
 			require.Equalf(t, tt.want, got, "Parallel(%v)", tt.args.operation)
 			if tt.wantErr {
-				expectedErr := []*error{&err2, &err3}
-				for _, e := range expectedErr {
+				for _, e := range tt.expectedErr {
 					require.ErrorAs(t, err, e)
 				}
 			}
-			time.Sleep(100 * time.Millisecond)
 		})
 	}
 }
